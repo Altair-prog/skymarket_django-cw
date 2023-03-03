88528e8337da3519e53ed1578102d83c6c4fd537
aleks <rezultat-24@yandex.ru>
2023-03-03 20:32:27 +0300

    commit initial

--- skymarket/users/serializers.py
+++ skymarket/users/serializers.py
@@ -4,27 +4,16 @@ from django.contrib.auth import get_user_model
 from users.models import CustomUser
 
 User = get_user_model()
-# TODO Здесь нам придется переопределить сериалайзер, который использует djoser
-# TODO для создания пользователя из за того, что у нас имеются нестандартные поля
 
 
 class UserRegistrationSerializer(BaseUserRegistrationSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['first_name',
-                  'last_name',
-                  'email',
-                  'password',
-                  'phone',
-                  'image']
+
+    class Meta(BaseUserRegistrationSerializer.Meta):
+        fields = ('email', 'first_name', 'last_name', 'password', 'phone', 'image')
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['first_name',
-                  'last_name',
-                  'email',
-                  'phone',
-                  'id',
-                  'image']
+
+    class Meta(BaseUserRegistrationSerializer.Meta):
+        model = User
+        fields = ('first_name', 'last_name', 'phone', 'image')
